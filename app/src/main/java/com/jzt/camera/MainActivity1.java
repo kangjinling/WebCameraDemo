6594ede11fce4c7fbf936183fdeac9c10ec78dcd
kangjinling <401205099@qq.com>
2018-10-23 20:23:13 +0800

    22222222222

--- app/src/main/java/com/jzt/camera/MainActivity1.java
+++ app/src/main/java/com/jzt/camera/MainActivity1.java
@@ -43,7 +43,7 @@ import java.util.Locale;
 
 import static android.os.Build.VERSION_CODES.M;
 
-// 1111111111111
+// 1111111111111222222222222222222222222
 public class MainActivity1 extends AppCompatActivity {
     private WebView webview;
     private ValueCallback<Uri[]> mFilePathCallback;
